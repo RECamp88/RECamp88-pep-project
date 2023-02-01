4ea4b486d32b658d5053b72ef4854c780fe791cc
Rachel Campbell <recamp254@gmail.com>
2023-02-01 09:32:23 +0000

    Added constructors & addAccount to AccountService

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -2,4 +2,22 @@ package Service;
 
 public class AccountService {
     
+    private AccountDAO accountDAO;
+
+    // no args constructor for creating a new AccountService with a new AccountDAO
+    public AccountService(){
+        accountDAO = new AccountDAO;
+    }
+
+    // constructor when accountDAO is provided
+    public AccountService (AccountDAO accountDAO){
+        this.accountDAO - accountDAO;
+    }
+
+    // using the AccountDAO to persist and account.  Account ID will not be provided. 
+    public Account addAccount(Account account){
+        return accountDAO.addAccount(account);
+    }
+
+    
 }
